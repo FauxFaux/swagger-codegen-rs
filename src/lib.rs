de7733af1545da553eda04a03acbcef08836e34b
Chris West (Faux) <git@goeswhere.com>
2018-06-08 16:58:00 +0100

    recursive descriptor code finally does something

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate failure;
 extern crate yaml_rust;
 
 use failure::Error;
+use failure::ResultExt;
 use std::collections::HashSet;
 use yaml_rust::yaml::Hash;
 use yaml_rust::Yaml;
@@ -37,16 +38,20 @@ pub fn go() -> Result<(), Error> {
 
     println!(
         "{:?}",
-        keys(doc
-            .as_hash()
-            .ok_or_else(|| format_err!("doc must be a hash"))?)?
+        keys(
+            doc.as_hash()
+                .ok_or_else(|| format_err!("doc must be a hash"))?
+        )?
     );
 
     let mut structs = Vec::new();
 
-    properties_to_fields(&mut structs, doc["definitions"]
-        .as_hash()
-        .ok_or_else(|| format_err!("no definitions"))?)?;
+    properties_to_fields(
+        &mut structs,
+        doc["definitions"]
+            .as_hash()
+            .ok_or_else(|| format_err!("no definitions"))?,
+    ).with_context(|_| format_err!("processing defintions"))?;
 
     for (path_url, path) in doc["paths"]
         .as_hash()
@@ -75,9 +80,10 @@ pub fn go() -> Result<(), Error> {
                 other => bail!("unsupported http method: {}", other),
             };
 
-            process_method(op
-                .as_hash()
-                .ok_or_else(|| format_err!("non-hash op body: {:?}", op))?)?;
+            process_method(
+                op.as_hash()
+                    .ok_or_else(|| format_err!("non-hash op body: {:?}", op))?,
+            )?;
         }
     }
     Ok(())
@@ -111,7 +117,6 @@ fn process_method(op: &Hash) -> Result<(), Error> {
 }
 
 fn properties_to_fields(new_structs: &mut Vec<Struct>, hash: &Hash) -> Result<Vec<Field>, Error> {
-
     let mut ret = Vec::new();
 
     for (field_name, field) in hash.into_iter() {
@@ -134,33 +139,53 @@ fn properties_to_fields(new_structs: &mut Vec<Struct>, hash: &Hash) -> Result<Ve
         current_keys.remove("required");
         current_keys.remove("x-nullable");
 
-        if current_keys.remove("allOf") {
-            current_keys.remove("type"); // must be object?
-        } else if current_keys.remove("type") || current_keys.contains("properties") {
-            match get_string(field, "type")? {
-                "object" => {
-                    if current_keys.remove("properties") {
-                        let new_struct = Struct {
-                            name: field_name.to_string(),
-                            description: description.to_string(),
-                            fields: properties_to_fields(new_structs, get_hash(field, "properties")?)?,
-                        };
+        if current_keys.remove("properties") {
+            if current_keys.remove("type") {
+                let object = get_string(field, "type")?;
+                ensure!(
+                    "object" == object,
+                    "field with `properties` must have no type, or type object, not {:?}",
+                    object
+                );
+            }
 
-                        new_structs.push(new_struct);
-                    } else {
-                        ensure!(
-                            current_keys.remove("additionalProperties"),
-                            "must have properties"
-                        );
-                    }
+            let new_struct = Struct {
+                name: field_name.to_string(),
+                description: description.to_string(),
+                fields: properties_to_fields(new_structs, get_hash(field, "properties")?)?,
+            };
+
+            new_structs.push(new_struct);
+        } else if current_keys.remove("additionalProperties") {
+            current_keys.remove("type");
+
+            // ???
+
+            // ?????
+            current_keys.remove("default");
+        } else if current_keys.remove("allOf") {
+            current_keys.remove("type"); // must be object?
+        } else if current_keys.remove("type") {
+            match get_string(field, "type")? {
+                "object" if current_keys.is_empty() => {
+                    // TODO: Total bullshit. No idea what this should do.
                 }
+                "object" => bail!("{}: type object, but no properties", field_name),
                 "array" => {
-                    ensure!(current_keys.remove("items"), "{}: arrays must have items", field_name);
+                    ensure!(
+                        current_keys.remove("items"),
+                        "{}: arrays must have items",
+                        field_name
+                    );
+                    current_keys.remove("default");
                 }
                 "integer" => {
                     current_keys.remove("format");
                     current_keys.remove("minimum");
                     current_keys.remove("maximum");
+                    current_keys.remove("default");
+                }
+                "number" => {
                     current_keys.remove("default");
                 }
                 "boolean" => {
