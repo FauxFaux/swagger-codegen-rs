36b61cfc3dac28f06203479630a3c9ade2ab784d
Chris West (Faux) <git@goeswhere.com>
2018-06-26 14:53:19 +0100

    getting a bit more serious about rendering

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@ extern crate yaml_rust;
 mod render;
 mod swagger;
 
+use std::io::Write;
+
 use failure::Error;
 use failure::ResultExt;
 
@@ -18,6 +20,9 @@ use swagger::NamingType;
 pub fn go() -> Result<(), Error> {
     let doc = yaml_rust::YamlLoader::load_from_str(include_str!("../examples/docker.yaml"))?;
     let doc = &doc[0];
+
+    let stdout = ::std::io::stdout();
+    let mut stdout = stdout.lock();
 
     let (endpoints, definitions) =
         swagger::load(
@@ -30,27 +35,24 @@ pub fn go() -> Result<(), Error> {
         ).with_context(|_| format_err!("loading full types from yaml"))?;
 
     for (name, naming) in definitions {
-        use heck::MixedCase;
+        use heck::SnakeCase;
 
         match naming {
             NamingType::Field(fields) => {
-                println!("struct {} {{", name);
+                writeln!(stdout, "struct {} {{", name)?;
                 for field in fields {
-                    println!(
+                    writeln!(
+                        stdout,
                         "    {}: {},",
-                        field.name.to_mixed_case(),
+                        field.name.to_snake_case(),
                         render::render(&field.data_type,)
-                    );
+                    )?;
                 }
-                println!("}}");
+                writeln!(stdout, "}}")?;
             }
 
-            NamingType::Enum(values, _default) => {
-                println!("enum {} {{", name);
-                for value in values {
-                    println!("    {},", value);
-                }
-                println!("}}");
+            NamingType::Enum(values, default) => {
+                render::render_enum(&mut stdout, &name, &values, default.as_ref())?
             }
         }
     }
--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,5 +1,9 @@
-use cast::i64;
+use std::io::Write;
 
+use cast::i64;
+use failure::Error;
+
+use std::collections::HashSet;
 use swagger::DataType;
 use swagger::IntegerFormat;
 use swagger::NamedType;
@@ -62,5 +66,73 @@ fn render_simple(simple: &DataType) -> String {
                 format_bits.max(max_bits)
             )
         }
+    }
+}
+
+pub fn render_enum<W: Write>(
+    mut into: W,
+    name: &str,
+    values: &[String],
+    default: Option<&String>,
+) -> Result<(), Error> {
+    let mut used = HashSet::with_capacity(values.len());
+
+    writeln!(
+        into,
+        "#[derive(Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]"
+    )?;
+    writeln!(into, "enum {} {{", name)?;
+    for value in values {
+        let rusty_name = rustify_enum_constant(value);
+
+        writeln!(into, "    #[serde(rename = \"{}\")]", value)?;
+        writeln!(into, "    {},", rusty_name)?;
+        if !used.insert(rusty_name) {
+            bail!("generated duplicate rusty name for {:?}", values);
+        }
+    }
+    writeln!(into, "}}")?;
+
+    if let Some(default) = default {
+        writeln!(into)?;
+        writeln!(into, "impl Default for {} {{", name)?;
+        writeln!(into, "    fn default() -> Self {{")?;
+        writeln!(into, "        Self::{}", rustify_enum_constant(default))?;
+        writeln!(into, "    }}")?;
+        writeln!(into, "}}")?;
+    }
+    Ok(())
+}
+
+fn rustify_enum_constant(name: &str) -> String {
+    let without_non_identifier: String =
+        name.chars().filter(|c| c.is_ascii_alphanumeric()).collect();
+
+    if without_non_identifier.is_empty() {
+        return "Empty".to_string();
+    }
+
+    use heck::CamelCase;
+
+    let potentially_valid = if without_non_identifier
+        .chars()
+        .next()
+        .expect("just checked it wasn't empty..")
+        .is_ascii_digit()
+    {
+        format!("N{}", without_non_identifier)
+    } else {
+        without_non_identifier
+    };
+
+    potentially_valid.to_camel_case()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn name_munging() {
+        use super::rustify_enum_constant;
+        assert_eq!("Foobar", rustify_enum_constant("foo.bar")); // not ideal
     }
 }
