4b38eb605ba42b0c746f29669c3c58add9ca8f0a
Chris West (Faux) <git@goeswhere.com>
2018-06-29 20:15:29 +0100

    test writes data to a file

--- src/lib.rs
+++ src/lib.rs
@@ -12,14 +12,12 @@ mod swagger;
 use failure::Error;
 use failure::ResultExt;
 
+use std::io::Write;
 use swagger::NamingType;
 
-pub fn go() -> Result<(), Error> {
+pub fn go<W: Write>(mut into: W) -> Result<(), Error> {
     let doc = yaml_rust::YamlLoader::load_from_str(include_str!("../examples/docker.yaml"))?;
     let doc = &doc[0];
-
-    let stdout = ::std::io::stdout();
-    let mut stdout = stdout.lock();
 
     let (endpoints, definitions) =
         swagger::load(
@@ -31,17 +29,19 @@ pub fn go() -> Result<(), Error> {
                 .ok_or_else(|| format_err!("no paths"))?,
         ).with_context(|_| format_err!("loading full types from yaml"))?;
 
-    render::render_definitions(&mut stdout, &definitions)?;
+    render::render_definitions(&mut into, &definitions)?;
 
-    render::render_endpoints(&mut stdout, &endpoints)?;
+    render::render_endpoints(&mut into, &endpoints)?;
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     #[test]
     fn test() {
-        ::go().unwrap()
+        ::go(fs::File::create("a.rs").unwrap()).unwrap()
     }
 }
