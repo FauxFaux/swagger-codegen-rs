eae25e00e1323e9e25ca89c9dbe39f9f56856fd7
Chris West (Faux) <git@goeswhere.com>
2018-07-01 13:27:23 +0100

    still unstable around the broken struct

--- demo/src/docker_gen.rs
+++ demo/src/docker_gen.rs
@@ -64,7 +64,7 @@ struct ConfigSpec {
 }
 
 #[derive(Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
-struct ContainerArchiveInfo {
+struct ContainerArchive {
     message: String,
     message: String,
 }
